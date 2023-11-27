a6f2cd6222e417f60d4b61baf703bf8f29f2c1d5
sebektokoks <sebektokoks@gmail.com>
2023-11-27 15:36:10 +0100

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,43 @@
-console.log('elo')
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+
+// Połączenie z bazą danych MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'users',
+    port: 3308
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Błąd połączenia z bazą danych:', err);
+    } else {
+        console.log('Połączenie z bazą danych MySQL udane!');
+    }
+});
+
+// Przykładowy endpoint GET
+app.get('/dane', (req, res) => {
+    // Przykładowe zapytanie do bazy danych
+    connection.query('SELECT * FROM userzy', (error, results, fields) => {
+        if (error) {
+            console.error('Błąd zapytania do bazy danych:', error);
+            res.status(500).json({ error: 'Błąd zapytania do bazy danych' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Uruchomienie serwera na porcie 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serwer uruchomiony na porcie ${PORT}`);
+});
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,57 @@
-function App() {
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [dane, setDane] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/dane'); // Endpoint na serwerze
+        const data = await response.json();
+        setDane(data);
+      } catch (error) {
+        console.error('Błąd pobierania danych:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Pusta zależność, aby efekt był uruchamiany tylko raz podczas zamontowania komponentu
+
+  const renderTable = () => {
+    if (!dane || dane.length === 0) {
+      return <p>Brak danych do wyświetlenia</p>;
+    }
+
+    const headers = Object.keys(dane[0]);
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            {headers.map(header => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dane.map((row, index) => (
+            <tr key={index}>
+              {headers.map(header => (
+                <td key={header}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <h1>Dane z serwera:</h1>
+      {renderTable()}
+    </div>
   );
-}
+};
 
 export default App;
